3652d7c4e8057b63aaec17903e7327d880e85812
Pyromage <the.pyromagi@gmail.com>
2019-07-31 13:10:29 +1000

    Added high score system and basic score display text
    (There is no high score display yet)

---
+++ Industrial Maintenance/Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+	[SerializeField] private Text m_scoreText;
+	[SerializeField] private Text m_firstHighScoreText;
+	[SerializeField] private Text m_secondHighScoreText;
+	[SerializeField] private Text m_thirdHighScoreText;
+
+	// Update is called once per frame
+	void Update()
+    {
+		//update score display
+		m_scoreText.text = ScoreManager.GetScore().ToString();
+    }
+}
