0e869eb4e88c3f864aa826012ea90e3c5c2ea206
Pyromage <the.pyromagi@gmail.com>
2019-08-01 10:23:22 +1000

    Wrote some basic machines code, not yet working
    Also made some folders for future use

---
+++ Industrial Maintenance/Assets/Scripts/Machines/BaseMachine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MachineIssue
+{
+	FIXED, 
+	ON_FIRE,
+	DUST_PLUMES,
+	SPARKING
+}
+
+public class BaseMachine : MonoBehaviour
+{
+	//Whether or not the machine is currently working
+	protected bool m_isWorking;
+
+	//If the machine is not working, what the issue with it is
+	protected MachineIssue m_issue;
+
+	/// <summary>
+	/// Calls for the machine to be broken
+	/// </summary>
+	/// <param name="issue">What the issue is</param>
+	public void BreakMachine(MachineIssue issue)
+	{
+		if (issue == MachineIssue.FIXED) //if the issue given is FIXED, don't break the machine
+			return;
+		if (!m_isWorking) //if the machine is already broken, don't rebreak it
+			return;
+		m_isWorking = false;
+		m_issue = issue;
+	}
+
+	/// <summary>
+	/// Calls for the machine to return to a working state
+	/// </summary>
+	protected void FixMachine()
+	{
+		m_isWorking = true;
+		m_issue = MachineIssue.FIXED;
+	}
+
+	/// <summary>
+	/// Gets the working state of the machine
+	/// </summary>
+	/// <returns>The working state</returns>
+	public bool GetWorking()
+	{
+		return m_isWorking;
+	}
+}
---
+++ Industrial Maintenance/Assets/Scripts/Managers/MachineManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineManager : MonoBehaviour
+{
+	[Header("Machines")]
+		[Tooltip("The object of the woodchipper")]
+		[SerializeField] private BaseMachine m_woodchipper;
+		[Tooltip("The object of the press")]
+		[SerializeField] private BaseMachine m_press;
+		[Tooltip("The object of the generator")]
+		[SerializeField] private BaseMachine m_generator;
+
+	//whether or not the factory is currently producing boxes
+	private bool m_producingBoxes = true;
+
+	[Header("Timers")]
+		[Tooltip("How long between machine breakings")]
+		[SerializeField] private float m_timeBetweenBreaks;
+		//The current timer of the machine breaking
+		private float m_breakTimer = 0.0f;
+	
+		[Tooltip("How long it takes to produce a box")]
+		[SerializeField] private float m_timeBetweenBoxes;
+		//the current timer of box production
+		private float m_boxTimer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Check if the factory can produce boxes
+		m_producingBoxes = CheckMachines();
+		if(m_producingBoxes)
+		{
+			//Increment timer
+			m_boxTimer += Time.deltaTime;
+
+			//If timer reaches threshold, produce a box
+			if(m_boxTimer >= m_timeBetweenBoxes)
+			{
+				//Reset timer and add score
+				m_boxTimer -= m_timeBetweenBoxes;
+				Debug.Log("A box was produced!");
+				ScoreManager.AddScore(1);
+			}
+		}
+
+		m_breakTimer += Time.deltaTime;
+		if(m_breakTimer >= m_timeBetweenBreaks)
+		{
+			m_breakTimer -= m_timeBetweenBreaks;
+			BreakMachine();
+		}
+    }
+
+	/// <summary>
+	/// Checks if all machines are functional
+	/// </summary>
+	/// <returns>Whether or not all machines are working</returns>
+	private bool CheckMachines()
+	{
+		if (!m_woodchipper.GetWorking())
+			return false;
+		if (!m_press.GetWorking())
+			return false;
+		if (!m_generator.GetWorking())
+			return false;
+		return true;
+	}
+
+	/// <summary>
+	/// Generates a random issue for a machine
+	/// </summary>
+	/// <returns>A random MachineIssue</returns>
+	private MachineIssue GenerateIssue()
+	{
+		return (MachineIssue)Random.Range(0, 4);
+	}
+
+	/// <summary>
+	/// Breaks a random machine, has a chance to generate a 'FIXED' issue, or break an already broken machine
+	/// in which case, nothing will happen
+	/// </summary>
+	private void BreakMachine()
+	{
+		int machineToBreak = Random.Range(0, 3);
+		MachineIssue issue = GenerateIssue();
+		switch(machineToBreak)
+		{
+			case (0):
+				m_woodchipper.BreakMachine(issue);
+				Debug.Log("Woodchipper broke with issue " + issue.ToString());
+				break;
+			case (1):
+				m_press.BreakMachine(issue);
+				Debug.Log("Press broke with issue " + issue.ToString());
+				break;
+			case (2):
+				m_generator.BreakMachine(issue);
+				Debug.Log("Generator broke with issue " + issue.ToString());
+				break;
+			default:
+				Debug.LogError("Something went horribly wrong with Random.Range");
+				break;
+		}
+	}
+}
